09db628bfe1556a116a1987473e970eb154fc8d4
Corinne <corinnewhite@live.co.uk>
2018-12-01 09:53:14 +0000

    Seasons route created
    Get list of seasons and tvshow they belong to

--- src/Controller/TvShowsController.php
+++ src/Controller/TvShowsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\TvShows;
+use App\Entity\Seasons;
 
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +15,19 @@ class TvShowsController extends AbstractController {
    * @Route("/tvshows", name="tvShows", methods={"GET"})
    */
   public function showTvShows() {
-    $tvShows = $this->getDoctrine()->getRepository(TvShows::class)->findAll();
-    
+    $tvShows = $this->getDoctrine()->getRepository(TvShows::class)->findAll();    
 
     return $this->render('tvshowlist.html.twig', array('tvShows' => $tvShows));
+  }
+
+  /**
+   * @Route("tvshows/{id}", name="seasons", methods={"GET"})
+   */
+  public function showSeasons($id) {
+    $tvShow = $this->getDoctrine()->getRepository(TvShows::class)->find($id);
+    $seasons = $this->getDoctrine()->getRepository(Seasons::class)->findBy(['tv_show' => $id]);
+
+    return $this->render('seasonlist.html.twig', array('id' => $id, 'seasons' => $seasons, 'tvShow' => $tvShow));
   }
 }
 ?>
