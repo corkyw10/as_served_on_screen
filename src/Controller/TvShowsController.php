abbb263c1a7ff417a04f42370f482752676736b1
Corinne <corinnewhite@live.co.uk>
2018-12-03 14:48:46 +0000

    Route added for Episode List
    Renders simple template for listing episodes

--- src/Controller/TvShowsController.php
+++ src/Controller/TvShowsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\TvShows;
 use App\Entity\Seasons;
+use App\Entity\Episodes;
 
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,6 +29,15 @@ class TvShowsController extends AbstractController {
     $seasons = $this->getDoctrine()->getRepository(Seasons::class)->findBy(['tv_show' => $id]);
 
     return $this->render('seasonlist.html.twig', array('id' => $id, 'seasons' => $seasons, 'tvShow' => $tvShow));
+  }
+
+  /**
+   * @Route("tvshows/{seasonId}/{episodeId}", name="episodes", methods={"GET"})
+   */
+  public function showEpisodes($seasonId, $episodeId) {
+    $episodes = $this->getDoctrine()->getRepository(Episodes::class)->findBy(['season' => $seasonId]);
+
+    return $this->render('episodelist.html.twig', array('episodes' => $episodes));
   }
 }
 ?>
