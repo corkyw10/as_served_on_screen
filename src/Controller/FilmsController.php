4befb6919bb2f655f59724f4844b404709277c54
Corinne <corinnewhite@live.co.uk>
2018-12-04 15:27:12 +0000

    Route added for film restuarant list

--- src/Controller/FilmsController.php
+++ src/Controller/FilmsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Films;
+use App\Entity\FilmRestaurants;
 
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +15,18 @@ class FilmsController extends AbstractController {
    * @Route("/films", name="films", methods={"GET"})
    */
   public function showFilms() {
-    $films = $this->getDoctrine()->getRepository(Films::class)->findAll();
-    
+    $films = $this->getDoctrine()->getRepository(Films::class)->findAll();    
 
     return $this->render('filmlist.html.twig', array('films' => $films));
+  }
+
+  /**
+   * @Route("/films/{filmId}", name="filmrestaurants", methods={"GET"})
+   */
+  public function showFilmRestaurants($filmId) {
+    $restaurants = $this->getDoctrine()->getRepository(FilmRestaurants::class)->findBy(['film' => $filmId]);
+
+    return $this->render("filmrestaurantslist.html.twig", array('restaurants' => $restaurants));  
   }
 }
 ?>
