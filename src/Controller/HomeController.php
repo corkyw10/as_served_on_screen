8ac8bcb235748472699627d69429ea01a6b8f7a1
Corinne <corinnewhite@live.co.uk>
2018-12-04 17:34:04 +0000

    Home route given name and method

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController {
   /**
-   * @Route("/home")
+   * @Route("/home", name="home", methods={"GET"})
    */
   public function index() {
     return $this->render('cover.html.twig');
