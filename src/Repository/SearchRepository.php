85671d576054e28b24176e0226d0deaf3a961b2a
Corinne <corinnewhite@live.co.uk>
2019-01-14 19:44:20 +0000

    Search results and Next Ten Search Results queries added

---
+++ src/Repository/SearchRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Search;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Search|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Search|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Search[]    findAll()
+ * @method Search[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SearchRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Search::class);
+    }
+
+    /**
+     * @param $searchTerm
+     * @return SearchResults[]
+     */
+    public function getSearchResults($searchTerm) {
+        return $this->createQueryBuilder('s')
+            ->where('s.name LIKE :searchTerm')
+            ->setParameter('searchTerm', '%'.addcslashes($searchTerm, '%_').'%')
+            ->orderBy('s.name', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @param $searchTerm, $limit, $offset
+     * @return SearchResults
+     */
+    public function getNextTenSearchResults($searchTerm, $limit, $offset) {
+        return $this->createQueryBuilder('s')
+        ->where('s.name LIKE :searchTerm')
+        ->setParameter('searchTerm', '%'.addcslashes($searchTerm, '%_').'%')
+        ->orderBy('s.name', 'ASC')
+        ->setFirstResult($limit)
+        ->setMaxResults($offset)
+        ->getQuery()
+        ->getResult();
+    }
+
+    // /**
+    //  * @return Search[] Returns an array of Search objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Search
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
