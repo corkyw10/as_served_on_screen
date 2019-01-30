092d6edc2999fdd8de40a21d73f42f2a3b37cc11
Corinne <corinnewhite@live.co.uk>
2019-01-30 14:44:03 +0100

    return search results function

--- src/Form/SearchBoxType.php
+++ src/Form/SearchBoxType.php
@@ -7,16 +7,49 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+use App\Entity\Search;
+use App\Repository\SearchRepository;
 
 
 
 class SearchBoxType extends AbstractType {
 
+  protected $form;
+
+  private $searchRepository;
+
+  public function __construct(SearchRepository $searchRepository) {
+    $this->searchRepository = $searchRepository;
+  }
+
   public function buildForm(FormBuilderInterface $builder, array $options) {
+
+    /* Builds a search bar that must have a search term of at least 3 characters
+     and not blank*/
+
     $builder
       ->setMethod('GET')
-      ->add('search', SearchType::class)
+      ->add('search', SearchType::class, [
+        'constraints' => [
+          new Length(['min' => 3]),
+          new notBlank(),]
+      ])
       ->add('Submit', SubmitType::class);
+  }
+
+  public function returnSearchResults($form) {
+
+    /* Returns an array with search results and search term to populate
+     template renderer */
+
+    $searchTerm = $form->getData();
+    $searchTerm = $searchTerm['search'];
+    $results = $this->searchRepository->getSearchResults($searchTerm);
+    $templateParameters = array('results' => $results, 'searchTerm' => $searchTerm);
+    return $templateParameters;
   }
 }
 
